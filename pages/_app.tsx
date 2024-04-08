2cb1e2994bde1162a3a2b49f3217c656ed081c7c
jawkjiang <2919036369@qq.com>
2024-04-08 21:06:58 +0800

    提交文件

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import WalletContextProvider from '../components/WalletContextProvider'
+
+function MyApp({ Component, pageProps }) {
+  return  <WalletContextProvider><Component {...pageProps} /></WalletContextProvider>
+}
+
+export default MyApp
